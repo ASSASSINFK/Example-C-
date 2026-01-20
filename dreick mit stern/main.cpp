d4627673d7bdde16fb441e50d54c96140e562e45
Feramiz Kılıç <95533611+ASSASSINFK@users.noreply.github.com>
2026-01-20 12:35:26 +0300

    half triangle with stars

---
+++ dreick mit stern/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	
+	
+	int i,j,n;
+	do{
+	
+	cin >> n;
+	
+	
+	for(i=1;i<=n;i++){
+		
+        for(j=1;j<=i;j++){
+        	
+        	cout << "*";
+		
+		
+			} cout << endl;
+	
+}
+}while(n!=0);
+	
+	return 0;
+}
